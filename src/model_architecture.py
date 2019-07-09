c9bae10e3cb41cc5c514006863d3765c401d5959
angela-lr <angela.lopez.delrio@gmail.com>
2019-07-09 15:33:23 +0200

    finished model_architecture task 1 testing

--- src/model_architecture.py
+++ src/model_architecture.py
@@ -15,7 +15,7 @@ import h5py
 import tensorflow as tf
 import keras
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input, Flatten, Conv1D
+from keras.layers import Dense, Dropout, Input, Flatten, Conv1D, MaxPooling1D, concatenate
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 from keras.backend.tensorflow_backend import set_session 
@@ -42,6 +42,7 @@ def building_3dense_model_task1(max_len, dict_size, number_neurons, n_class, dro
     dense_seq3 = Dense(number_neurons[2], activation='relu')(dropout_seq2)
     dropout_seq3 = Dropout(drop_hid)(dense_seq3)
     main_dense = Dense(n_class, activation=final_act)(dropout_seq3)
+    model = Model(inputs=[input_seq], outputs=[main_dense])
     print(model.summary())
     
     # saving the model
@@ -50,8 +51,7 @@ def building_3dense_model_task1(max_len, dict_size, number_neurons, n_class, dro
     model.save(file_model)
     return model
 
-def building_1convdense_model_task1(max_len, dict_size, nnumber_neurons, n_class, drop_per, drop_hid, n_filt, kernel_size,
-                                    final_act, folder):
+def building_1convdense_model_task1(max_len, dict_size, number_neurons, n_class, drop_per, drop_hid, n_filt, kernel_size, final_act, folder):
     """"Builds a model with a convolutional layer and three Dense layers whose number of neurons are specified in decreasing order in number_neurons"""
     input_seq = Input(shape=(max_len, dict_size), dtype='float32')
     dropout_seq = Dropout(drop_per)(input_seq)
@@ -65,6 +65,7 @@ def building_1convdense_model_task1(max_len, dict_size, nnumber_neurons, n_class
     dense_seq3 = Dense(number_neurons[2], activation='relu')(dropout_seq2)
     dropout_seq3 = Dropout(drop_hid)(dense_seq3)
     main_dense = Dense(n_class, activation=final_act)(dropout_seq3)
+    model = Model(inputs=[input_seq], outputs=[main_dense])
     print(model.summary())
     
     # saving the model
@@ -73,8 +74,7 @@ def building_1convdense_model_task1(max_len, dict_size, nnumber_neurons, n_class
     model.save(file_model)
     return model
 
-def building_stackconv_model_task1(max_len, dict_size, nnumber_neurons, n_class, drop_per, drop_hid, n_filt, kernel_size,
-                                    pool_size, final_act, folder):
+def building_stackconv_model_task1(max_len, dict_size, number_neurons, n_class, drop_per, drop_hid, n_filt, kernel_size, pool_size, final_act, folder):
     """"Builds a model with a stack of convolutional layers and three Dense layers whose number of neurons are specified in decreasing order in number_neurons"""
     input_seq = Input(shape=(max_len, dict_size), dtype='float32')
     dropout_seq = Dropout(drop_per)(input_seq)
@@ -91,13 +91,15 @@ def building_stackconv_model_task1(max_len, dict_size, nnumber_neurons, n_class,
     #Pooling to reduce dimensions
     pooled = MaxPooling1D(pool_size=pool_size, strides=None, padding='same')(merged_conv)    
     #Denses
-    dense_seq1 = Dense(number_neurons[0], activation='relu')(c1)
+    dense_seq1 = Dense(number_neurons[0], activation='relu')(pooled)
     dropout_seq1 = Dropout(drop_hid)(dense_seq1)
     dense_seq2 = Dense(number_neurons[1], activation='relu')(dropout_seq1)
     dropout_seq2 = Dropout(drop_hid)(dense_seq2)
     dense_seq3 = Dense(number_neurons[2], activation='relu')(dropout_seq2)
     dropout_seq3 = Dropout(drop_hid)(dense_seq3)
     main_dense = Dense(n_class, activation=final_act)(dropout_seq3)
+    model = Model(inputs=[input_seq], outputs=[main_dense])
+
     print(model.summary())
     
     # saving the model
@@ -117,6 +119,7 @@ def building_2dense_model_task2(max_len, dict_size, number_neurons, n_class, dro
     dense_seq2 = Dense(number_neurons[1], activation='relu')(dropout_seq1)
     dropout_seq2 = Dropout(drop_hid)(dense_seq2)
     main_dense = Dense(n_class, activation=final_act)(dropout_seq2)
+    model = Model(inputs=[input_seq], outputs=[main_dense])
     print(model.summary())
     
     # saving the model
@@ -125,7 +128,7 @@ def building_2dense_model_task2(max_len, dict_size, number_neurons, n_class, dro
     model.save(file_model)
     return model
 
-def building_1convdense_model_task1(max_len, dict_size, nnumber_neurons, n_class, drop_per, drop_hid, n_filt, kernel_size,
+def building_1convdense_model_task2(max_len, dict_size, number_neurons, n_class, drop_per, drop_hid, n_filt, kernel_size,
                                     final_act, folder):
     """"Builds a model with a convolutional layer and two Dense layers whose number of neurons are specified in decreasing order in number_neurons"""
     input_seq = Input(shape=(max_len, dict_size), dtype='float32')
@@ -146,7 +149,7 @@ def building_1convdense_model_task1(max_len, dict_size, nnumber_neurons, n_class
     model.save(file_model)
     return model
 
-def building_stackconv_model_task1(max_len, dict_size, nnumber_neurons, n_class, drop_per, drop_hid, n_filt, kernel_size,
+def building_stackconv_model_task2(max_len, dict_size, nnumber_neurons, n_class, drop_per, drop_hid, n_filt, kernel_size,
                                     pool_size, final_act, folder):
     """"Builds a model with a stack of convolutional layers and three Dense layers whose number of neurons are specified in decreasing order in number_neurons"""
     input_seq = Input(shape=(max_len, dict_size), dtype='float32')
@@ -169,6 +172,7 @@ def building_stackconv_model_task1(max_len, dict_size, nnumber_neurons, n_class,
     dense_seq2 = Dense(number_neurons[1], activation='relu')(dropout_seq1)
     dropout_seq2 = Dropout(drop_hid)(dense_seq2)
     main_dense = Dense(n_class, activation=final_act)(dropout_seq2)
+    model = Model(inputs=[input_seq], outputs=[main_dense])
     print(model.summary())
     
     # saving the model
